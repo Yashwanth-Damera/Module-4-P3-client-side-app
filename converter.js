67c3e76a8571ef37e27d14ef269ee8712ddccbca
Yashwanth <yashwanthdamera6@gmail.com>
2020-03-06 17:30:09 -0600

    second commit

---
+++ converter.js
@@ -0,0 +1,6 @@
+<script>
+function weightConverter(valNum) {
+  document.getElementById("outputKilograms").innerHTML=valNum/2.2046;
+  document.getElementById("outputPounds").innerHTML=valNum*2.2046;
+}
+</script>
